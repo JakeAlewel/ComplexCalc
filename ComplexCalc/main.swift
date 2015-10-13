d234ce5b318f54f75794a1d8a7487335d45a2e9e
Jacob Alewel <jakealewel@ymail.com>
2015-10-13 11:49:14 -0700

    Basic calculator functions

--- ComplexCalc/main.swift
+++ ComplexCalc/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(first : Int, second : Int) -> Int {
+    return first + second;
+}
 
+func subtract(first : Int, second : Int) -> Int {
+    return first - second;
+}
+
+func multiply(first : Int, second : Int) -> Int {
+    return first * second;
+}
+
+func divide(first : Int, second : Int) -> Int {
+    return first / second;
+}
+
+func genericMathOperation(mathOperation : (Int, Int) -> Int, first : Int, second : Int) -> Int {
+    return mathOperation(first, second);
+}
+
+print(add(5, second: 5));
+print(subtract(5, second: 5));
+print(multiply(5, second: 5));
+print(divide(5, second: 5));
+print(genericMathOperation(subtract, first: 5, second: 5));
