b21074bf42a605662eb42948d38bc63d897ca536
Jacob Alewel <jakealewel@ymail.com>
2015-10-13 11:59:30 -0700

    Added add and multiply array operations

--- ComplexCalc/main.swift
+++ ComplexCalc/main.swift
@@ -33,3 +33,23 @@ print(subtract(5, second: 5));
 print(multiply(5, second: 5));
 print(divide(5, second: 5));
 print(genericMathOperation(subtract, first: 5, second: 5));
+
+func addArrayContents(values : [Int]) -> Int {
+    var total : Int = 0;
+    for value in values {
+        total = add(total, second: value);
+    }
+    return total;
+}
+
+func multiplyArrayContents(values : [Int]) -> Int {
+    var total : Int = 1;
+    for value in values {
+        total = multiply(total, second: value);
+    }
+    return total;
+}
+
+print(addArrayContents([1,2,3,4,5]));
+print(multiplyArrayContents([1,2,3,4,5]));
+
